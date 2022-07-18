b9c56514a8f1bebb5160e9c22a935b8a4f997856
petrixorism <theeconomist312@gmail.com>
2022-07-18 10:18:27 +0500

    view pager2

--- app/src/main/java/uz/gita/myapplication/ui/screen/SelectLangFragment.kt
+++ app/src/main/java/uz/gita/myapplication/ui/screen/SelectLangFragment.kt
@@ -2,15 +2,21 @@ package uz.gita.myapplication.ui.screen
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.myapplication.R
+import uz.gita.myapplication.databinding.SelectLanguageFragmentBinding
 
-class SelectLangFragment:Fragment(R.layout.select_language_fragment) {
+class SelectLangFragment : Fragment(R.layout.select_language_fragment) {
+
+    private val binding by viewBinding(SelectLanguageFragmentBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-
-
+        binding.radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+            findNavController().navigate(SelectLangFragmentDirections.actionFragmentSelectLangToOnboardParentFragment())
+        }
     }
 
 }
